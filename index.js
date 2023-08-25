d7ca74a180075ab0062d155b7ba45390f59020b3
unknown <smithbabawande@gmail.com>
2023-08-25 07:55:23 +0100

    Completed Assignmet

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+console.log(drivers);
+
+
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase();
+    });
+}
+
+
+console.log(findMatching(drivers, "Bobby"));
+
+console.log(findMatching(drivers, 'Sammy'));
+
+
+// const fuzzyMatch = findMatching;
+function fuzzyMatch(drivers, name){
+    
+    return drivers.filter(function(driver){
+        return    driver.toLowerCase().slice(0, name.length) === name.toLowerCase();
+    })
+}
+
+console.log(fuzzyMatch(drivers, "mm"))
+
+//check if name matches
+
+function matchName(drivers, name){
+    return drivers.filter(function (driver){
+        return driver.name.toLowerCase() == name.toLowerCase();
+    })
+}
+
+console.log(matchName(drivers, "Bobby"))
+
+
+
+
+
+
